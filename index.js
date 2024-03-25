ec74834d088ec4bb5980175fd8d48208aa9f1aad
AngelSiete <andraco660@gmail.com>
2024-03-25 17:37:41 +0100

    first routing + basic pug templates

--- index.js
+++ index.js
@@ -1,15 +1,25 @@
 import  express from 'express';
-
 const app = express();
+
+import router from "./routes/index.js";
 
 const port = process.env.PORT || 4000 ;
 
-app.get('/',(req,res)=>{
-    res.json({
-        id: 0
-    })
+
+app.set('view engine', 'pug');
+
+// Middlewares:
+
+app.use((req,res, next) => {
+    const currentYear = new Date().getFullYear();
+    res.locals.currentYear = currentYear;
+    res.locals.nombrePagina = 'Agencia de Viajes';
+    next();
 })
 
+app.use(express.static('public'));
+
+app.use('/', router);
 app.listen(port, () => {
     console.log(`connected to ${port}`)
 })
